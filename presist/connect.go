5c4453b5811bb371f8f28f699e1024f2acb92a99
unknown <youssefmaher46@yahoo.com>
2022-07-12 11:27:28 +0200

    commit

--- presist/connect.go
+++ presist/connect.go
@@ -6,16 +6,24 @@ import (
 	"os"
 
 	"github.com/jackc/pgx/v4"
+	"github.com/joho/godotenv"
 )
 
 var DB DbStore
 
 func Connect() DbStore {
-	conn, err := pgx.Connect(context.Background(), "postgres://postgres:145963201@localhost:5432/key_val")
+	err := godotenv.Load("./.env")
+	if err != nil {
+		fmt.Println("Couldn't load env file")
+		os.Exit(1)
+	}
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
+
 	DB.Client = conn
 	fmt.Println("DATABASE CONNECTED")
 	return DB
